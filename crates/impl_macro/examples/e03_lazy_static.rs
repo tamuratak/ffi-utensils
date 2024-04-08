f8d3caae5645ea3a4b442e19220c0563ed45e1fc
Takashi Tamura <tamuratak@users.noreply.github.com>
2024-04-08 13:22:20 +0900

    fixup! Add impl_macro

--- crates/impl_macro/examples/e03_lazy_static.rs
+++ crates/impl_macro/examples/e03_lazy_static.rs
@@ -245,13 +245,6 @@ lazy_static! {
         );
         m
     };
-    static ref HASHMAP: HashMap<u32, &'static str> = {
-        let mut m = HashMap::new();
-        m.insert(0, "foo");
-        m.insert(1, "bar");
-        m.insert(2, "baz");
-        m
-    };
 }
 
 fn register_class() -> &'static AnyClass {
